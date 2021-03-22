5bbd9f53948c4b0a0b1fcb596eab331dce5e475a
PrommarinPromngam <80934340+PrommarinPromngam@users.noreply.github.com>
2021-03-22 22:01:34 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1615982000,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bggame-sheet0.png",
+		"images/q-sheet0.png",
+		"images/q-sheet1.png",
+		"images/a-sheet0.png",
+		"images/a-sheet1.png",
+		"images/boxq-sheet0.png",
+		"images/line1-sheet0.png",
+		"images/line2-sheet0.png",
+		"images/line3-sheet0.png",
+		"images/line4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
